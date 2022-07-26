9057c1f4666a0bfffececbf3364f3d288d2a990e
MilaKropeczka <milakropeczka@gmail.com>
2022-07-26 11:08:51 +0200

    cwiczenie samemu

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
 const Post = (props) => {
-	const [showContent, setShowContent] = React.useState(false);
-
+	const [showContent, setShowContent] = React.useState(true);
 	const showMoreContent = () => {
-		setShowContent(true);
-	};
-	const showLessContent = () => {
 		setShowContent(false);
 	};
-
+	const showLessContent = () => {
+		setShowContent(true);
+	};
 	return (
-		<div>
-			<h3>{props.title}</h3>
+		<>
+			<h2>{props.title}</h2>
 			{showContent ? (
+				<>
+					<p>{props.shortContent}</p>
+					<button onClick={showMoreContent}>Pokaż więcej</button>
+				</>
+			) : (
 				<>
 					<p>{props.content}</p>
 					<button onClick={showLessContent}>X</button>
 				</>
-			) : (
-				<>
-					<p>{props.shortContent}</p>
-					<button onClick={showMoreContent}>Czytaj więcej</button>
-				</>
 			)}
-		</div>
+		</>
 	);
 };
 
 const Blog = () => {
 	const data = [
 		{
-			id: 1,
-			title: `Naleśniki z dzemem`,
-			content: `Do miski wsyp pełną szklankę mąki pszennej + jeszcze dwie łyżki mąki. Będzie to około 230 gramów mąki. Używam zazwyczaj mąki pszennej tortowej lub mąki uniwersalnej. Wbij trzy średniej wielkości jajka i wlej szklankę mleka oraz szklankę wody. Dodaj szczyptę soli i wlej cztery łyżki oleju roślinnego`,
+			title: `Przepis na naleśniki`,
 			shortContent: `Na słodko...`,
-			showContent: false,
+			id: 1,
+			content: `dlugi tekst lorem ipsum ador lorem ipsum ador lorem ipsum ador lorem ipsum ador `,
 		},
 		{
+			title: `Przepis na kurczaka w piekarniku`,
+			shortContent: `Na ostro...`,
 			id: 2,
-			title: `Kurczak w piekarniku`,
-			content: `Piekarnik nagrzać do 190 stopni. Kurczaka wysmarować solą i pieprzem, estragonem oraz czerwoną papryką. Do środka włożyć ćwiartki cytryny z ponacinaną skórką lub podziurawioną widelcem oraz cebulę, gałązki natki, rozmarynu i tymianku. Nogi kurczaka związać sznurkiem, całość polać i wysmarować roztopionym masłem.`,
-			shortContent: `Na słono...`,
-			showContent: true,
+			content: `dlugi tekst lorem ipsum ador lorem ipsum ador lorem ipsum ador lorem ipsum ador `,
 		},
 	];
+
 	const listPost = data.map((el) => <Post key={el.id} {...el} />);
+
 	return (
-		<div>
-			<h1>Przepisy</h1>
+		<>
+			<h1>Lista Przepisów</h1>
 			<hr />
 			{listPost}
-		</div>
+		</>
 	);
 };
 
