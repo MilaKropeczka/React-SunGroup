37fdcce562f5b9d16135caf93bbcd5859a86754d
MilaKropeczka <milakropeczka@gmail.com>
2022-07-26 12:44:22 +0200

    stylowanie inline

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -2,7 +2,16 @@ import React from 'react';
 import styles from './Header.module.css';
 
 const Header = () => {
-	return <header className={styles.header}> header </header>;
+	return (
+		<header className={styles.header}>
+			<div>
+                <input style={{
+                    width: `100px`
+                }} className={styles.input} type='text' placeholder='Szukaj...' />
+				<button>Szukaj</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
