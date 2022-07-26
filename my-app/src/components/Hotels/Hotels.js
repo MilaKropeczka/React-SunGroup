ad112907c8167c5b67aa1cfc1babbabf4285428c
MilaKropeczka <milakropeczka@gmail.com>
2022-07-26 12:29:29 +0200

    react struktura

---
+++ my-app/src/components/Hotels/Hotels.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hotel from './Hotel/Hotel';
+
+const Hotels = () => {
+	return (
+		<div>
+			hotels
+			<Hotel />
+			<Hotel />
+		</div>
+	);
+};
+
+export default Hotels;
