6ef361eb8d1ba776904b43def2f8838e14fcbc8a
MilaKropeczka <milakropeczka@gmail.com>
2022-07-28 10:22:45 +0200

    toggle menu

--- my-app/src/components/SunGroup/Nav/Nav.js
+++ my-app/src/components/SunGroup/Nav/Nav.js
@@ -2,25 +2,36 @@
 import React, { useState } from 'react';
 import Lists from './Lists/Lists';
 import img from '../../../assets/SunGroup/img/SunPage.svg';
-import Hamburger from 'hamburger-react';
 
 const Nav = () => {
-	const [menu, setMenu] = React.useState(true);
-	const showMenu = () => {
-		setMenu(false);
+	const [isActive, setActive] = useState('false');
+
+	const menuToggle = () => {
+		setActive(!isActive);
 	};
-	const [isOpen, setOpen] = useState(false);
+
 	return (
 		<nav>
 			<div>
 				<div className='navDeskop'>
 					<p className='hiddenDeskop'>
-						<i className='fa-solid fa-bars' onClick={showMenu}></i>
+						{/* <i className='fa-solid fa-bars' onClick={showMenu}></i> */}
+						<i
+							className='fa-solid fa-bars'
+							onClick={menuToggle}></i>
 					</p>
 					<img src={img} alt='Logo SunGroup' />
 					<Lists hidden='hiddenMobile' />
 				</div>
-				{menu ? (
+				<div
+					className={
+						'navMobile hiddenDeskop' +
+						' ' +
+						(isActive ? 'show' : null)
+					}>
+					<Lists />
+				</div>
+				{/* {setMenu ? (
 					<div className={`navMobile hiddenDeskop`}>
 						<Lists />
 					</div>
@@ -28,7 +39,7 @@ const Nav = () => {
 					<div className={`navMobile hiddenDeskop show`}>
 						<Lists />
 					</div>
-				)}
+				)} */}
 			</div>
 		</nav>
 	);
